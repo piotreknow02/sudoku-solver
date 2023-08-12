3f241af0f13670c2076af051265431f16a22648e
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2023-08-12 20:49:45 +0200

    still debugging

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod tokenizer;
 
 fn main() {
     let mut my_sudoku = Sudoku::from_json("testdata/1.json").unwrap();
-    // my_sudoku.solve();
+    my_sudoku.solve();
     println!("{}", &my_sudoku);
 
 }
--- src/solver.rs
+++ src/solver.rs
@@ -4,14 +4,27 @@ impl Sudoku {
     fn possible(&self, num: u8, position: (usize, usize)) -> bool {
         for y in 0..9 {
             if num == self.table[y][position.1].unwrap_or(0) {
+                println!("Number {} not possible {},{}", num, y, position.1);
                 return false
             }
         }
         for x in 0..9 {
             if num == self.table[position.0][x].unwrap_or(0) {
+                println!("Number {} not possible", num);
                 return false
             }
         }
+
+        let y0: usize = (position.1 / 3) * 3;
+        let x0: usize = (position.0 / 3) * 3;
+        for i in 0..3 {
+            for j in 0..3 {
+                if self.table[y0+i][x0+j].unwrap_or(0) == num {
+                    return false
+                }
+            }
+        }
+        println!("Number {} possible", num);
         true
     }
 
@@ -26,6 +39,7 @@ impl Sudoku {
                             self.table[y][x] = None;
                         }
                     }
+                    return;
                 }
             }
         }
